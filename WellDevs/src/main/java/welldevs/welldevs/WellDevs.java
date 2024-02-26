8e1046cec553956f476f1f4fefee0dc7db484837
XxDavidEspinosaxX <david.espinosa.fernandez@ieselcalamot.com>
2024-02-26 16:42:38 +0100

    Hola chiquis

---
+++ WellDevs/src/main/java/welldevs/welldevs/WellDevs.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package welldevs.welldevs;
+
+/**
+ *
+ * @author admin
+ */
+public class WellDevs {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
